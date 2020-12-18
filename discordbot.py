01ec9d93372e48c912c5051060e5a860c9b6b5b3
seeder3 <73010113+seeder3@users.noreply.github.com>
2020-12-18 23:20:35 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -51,6 +51,17 @@ async def russia(ctx):
     else:
         await ctx.send('https://www.youtube.com/watch?v=KOBMxU164Oc')   # 3%で音割れ
 
+# 60秒に一回ループ
+@tasks.loop(seconds=60)
+async def loop():
+    now = datetime.datetime.today()  # 現在時刻を取得
+    if((now.day == 29) and (now.hour == 15)):
+        channel = client.get_channel(784788831253037077)
+        await channel.send('音割れフェス開始! 期間中は/russiaでの音割れ出現確率2倍!(29日15時~月変更)') 
+    if((now.day == 1) and (now.hour == 0)):
+        channel = client.get_channel(784788831253037077)
+        await channel.send('音割れフェス終了')
+
 @bot.command()
 async def mahjong(ctx):
     await ctx.send('https://game.mahjongsoul.com/')
